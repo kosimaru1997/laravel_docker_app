015a809b23c3c5c74e91c11cd2b85fe88fe05c4d
kosimaru1997 <kosimaru1997@gmail.com>
2021-09-05 01:29:05 +0900

    fix destroy_route in site_index

--- backend/resources/views/site/index.blade.php
+++ backend/resources/views/site/index.blade.php
@@ -72,7 +72,7 @@
                     <a href="/site/{{$site->id}}/edit" class="mr-4">
                         <i class="far fa-edit fa-lg cursor-p"></i>
                     </a>
-                    <form class=".btn-dell" action="site/{{$site->id}}" method="POST">
+                    <form class=".btn-dell" action="site/{{$site->id}}/destroy" method="POST">
                         @csrf
                         <button type="submit" class="fas fa-trash fa-sm btn btn-outline-secondary cursor-p btn-dell mb-1 p-1"></button>
                     </form>
